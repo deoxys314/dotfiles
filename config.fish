58885da1c0cdb61159304d6ed4ca1627b1d1ff83
Cam.Rossington.MacAir <deoxys314@gmail.com>
2017-11-04 21:58:32 -0400

    Add ~/bin to PATH to install personal executables.

--- config.fish
+++ config.fish
@@ -4,5 +4,7 @@ source ~/deoxys314_dotfiles/fish/aliases.fish
 
 source ~/deoxys314_dotfiles/fish/functions.fish
 
+set PATH $PATH ~/bin
+
 # important environmental variables
 # set -g -x TERM screen-256color-bce
