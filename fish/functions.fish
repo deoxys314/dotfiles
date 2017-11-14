3a93d947b2fa8671cd30b7cb5dca1a7c08350d23
Cam.Rossington.debsrv <deoxys314@gmail.com>
2017-11-14 15:58:41 -0500

    Improve the `gi` function.

--- fish/functions.fish
+++ fish/functions.fish
@@ -68,7 +68,11 @@ function tm -d "Easier tmux usage."
 	end
 end
 
-function gi -d "Uses gitignore.io to create .gitignore files"
-	curl -L -s https://www.gitignore.io/api/$argv
+function gi -d "Uses gitignore.io to create .gitignore files" --argument-names 'tags'
+	if test -z $tags
+		echo "No arguments provided."
+		return 1
+	end
+	curl -L -s https://www.gitignore.io/api/$tags
 end
 
