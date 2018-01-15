5257437c3db5dfc3382c8624ae117602c2c75269
Cam.Rossington.MacAir <deoxys314@gmail.com>
2018-01-14 21:49:04 -0500

    Remove unused aliases and updated comments.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,7 +1,8 @@
 # This file contains aliases which are useful for all instances of fish
 
+# ls (a)ll (B) print unprintables (h) sizes in megabyte, et a;
+#    (s) file block size (l) long format
 alias lsa 'ls -aBhsl'
 
 # it's happened enough times
 alias :q exit
-
