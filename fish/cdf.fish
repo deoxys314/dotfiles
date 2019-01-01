d4dbf1c828dd07ad07936d2e214f55e4839e2bfe
Cam.Rossington.MacAir <deoxys314@gmail.com>
2019-01-01 18:18:43 -0500

    Add a check for operating system.
    As this function is being added to my dotfiles repository, a check was
    added to avoid awkward errors if unintentionally used on another system.

--- fish/cdf.fish
+++ fish/cdf.fish
@@ -1,4 +1,9 @@
 function cdf --description="If on OSX, `cd` to the directory the front Finder window is open to."
+	# first we check if we're on OSX
+	if not test (uname -s) = "Darwin"
+		echo "We are not on OSX." >&2
+		return 3
+	end
 	set --local target (osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
 	if test -n target
 		echo "Changing directory to: $target"
