3d8392db3ee1a9ad7f1cc5382f45348e519210bf
Cam.Rossington.MacAir <deoxys314@gmail.com>
2019-01-01 18:16:36 -0500

    Add description to cdf function.

--- fish/cdf.fish
+++ fish/cdf.fish
@@ -1,4 +1,4 @@
-function cdf
+function cdf --description="If on OSX, `cd` to the directory the front Finder window is open to."
 	set --local target (osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
 	if test -n target
 		echo "Changing directory to: $target"
