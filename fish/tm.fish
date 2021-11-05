e152c2e464b668585389baf229e9c71642ec007f
Cameron Rossington <cameron.rossington@eagleview.com>
2021-11-05 12:32:28 -0400

    Add more flexibility to `tm` command.
    Now will attach to extant sessions easily, if it can find a match for
    that session.
    
    This can cause collisions, so don't name a session "ls", I guess. Or,
    use the alternate form, `tm attach -t ls`, which will still work - if
    the number of arguments is greater than one, it will simply forwatd the
    command to tmux.

--- fish/tm.fish
+++ fish/tm.fish
@@ -1,6 +1,8 @@
 function tm --description="Easier tmux usage."
 	if test (count $argv) -eq 0
 		tmux attach; or tmux new -s "main"
+	else if test (count $argv) -eq 1; and tmux has-session -t $argv 2> /dev/null
+		tmux attach -t $argv
 	else
 		tmux $argv
 	end
