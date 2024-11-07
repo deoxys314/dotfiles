8b04c5c39023301608633325ce75e75586a3b665
Cameron Rossington <cameron.rossington@eagleview.com>
2024-11-07 08:31:38 -0500

    Fix indentation

--- profile.ps1
+++ profile.ps1
@@ -72,16 +72,16 @@ Function Reload-Profile {
 
 
 Function List-EmptyDirectories {
-	# Assumes you are in the correct directory unless told otherwise
-	Param (
-		[Parameter(mandatory)] $path
-	)
+    # Assumes you are in the correct directory unless told otherwise
+    Param (
+        [Parameter(mandatory)] $path
+    )
 
-	Get-ChildItem $path -Directory -Recurse -ErrorAction 'ignore' `
-	 | Where-Object { 
-		 ($_.GetFileSystemInfos().Count -eq 0) -and ($_.FullName -notmatch '\\\.')
-		 } `
-	 | ForEach-Object { $_.FullName }
+    Get-ChildItem $path -Directory -Recurse -ErrorAction 'ignore' `
+     | Where-Object { 
+         ($_.GetFileSystemInfos().Count -eq 0) -and ($_.FullName -notmatch '\\\.')
+         } `
+     | ForEach-Object { $_.FullName }
 }
 
 
