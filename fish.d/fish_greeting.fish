0f1f5f8d00c2c0c834685a48c5384ec790a1bfc4
Cameron Rossington <cameron.rossington@eagleview.com>
2024-12-12 16:07:48 -0500

    Update qotd function to be more flexible.

--- fish.d/fish_greeting.fish
+++ fish.d/fish_greeting.fish
@@ -1,7 +1,7 @@
 function fish_greeting
 	neofish
 
-	quote_of_the_day (git -C (status dirname) rev-parse --show-toplevel)'/qotd.txt' true
+	quote_of_the_day --print-header (git -C (status dirname) rev-parse --show-toplevel)'/qotd.txt'
 	
 	year_progress (math "0$COLUMNS" - 6)
 end
--- fish.d/quote_of_the_day.fish
+++ fish.d/quote_of_the_day.fish
@@ -1,18 +1,27 @@
 function quote_of_the_day \
-	--description 'Print out a random quote of the day from a provided file. File is expected to be one quote per line with a tab between the quote and the source (if any).' \
-	--argument-names quote_file print_header
+	--description 'Print out a random quote of the day from a provided file. File is expected to be one quote per line with a tab between the quote and the source (if any).'
 
-	if not test -e "$quote_file"
+
+	argparse (fish_opt --short p --long print-header) (fish_opt --short w --long width --required-val) -- $argv
+	or return
+
+	if not test -e "$argv[1]"
 		printf 'ERROR: Cannot find file %s\n' "$quote_file" >&2
 		return
 	end
 
-	if test -n "$print_header"
+	if set -ql _flag_print_header
 		echo (set_color yellow --underline)'Quote of the Day'(set_color normal)
 	end
 
-	set --local quotes (cat "$quote_file")
-	set --local n_quotes (count < "$quote_file")
-	set --local quote_idx (math (date '+%j') '%' "$n_quotes")
-	printf '%s\n' $quotes[$quote_idx] | sed 's/\t/\n\t- /' | fold -w 80 -s
+	if set -ql _flag_width
+		set --function width $_flag_width
+	else
+		set --function width 80
+	end
+
+	set --local quotes (cat "$argv[1]")
+	set --local n_quotes (count < "$argv[1]")
+	set --local quote_idx (math (date '+%V') '*' (date '+%j') '%' "$n_quotes")
+	printf '%s\n' $quotes[$quote_idx] | sed 's/\t/\n\t- /' | fold -w "$width" -s
 end
