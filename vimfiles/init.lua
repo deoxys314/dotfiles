56933a9b307837a383300ed4efad3dc4badb81db
Cameron Rossington <cameron.rossington@eagleview.com>
2025-04-24 09:24:30 -0400

    Update lualine style

--- vimfiles/init.lua
+++ vimfiles/init.lua
@@ -103,6 +103,10 @@ require('lazy').setup({
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {
+            options = {
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+            },
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff' },
